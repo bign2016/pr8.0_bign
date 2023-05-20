780943257c878484c6d3775bbe645d1ef386f97e
bign2016 <134098645+bign2016@users.noreply.github.com>
2023-05-20 09:19:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import random
 
+def password()
+  a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  b = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  c = ['1','2','3','4','5','6','7','8','9','0']
+  otvet = ''
+  for i in range(11):
+    otvet += random.choice(a + b + c)
+  return otvet
 
-13456879546213
+d = pssword()
+print('Пароль:',d)
+    
+
 
 
